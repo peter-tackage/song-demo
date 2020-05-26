91695ac331046e4248b14e26744a3045c373cc08
peter <peter.tackage@gmail.com>
2020-05-26 16:11:17 +0200

    Initial API skeleton

---
+++ app/src/main/java/com/petertackage/songapidemo/api/HeartThisAtApi.kt
@@ -0,0 +1,51 @@
+package com.petertackage.songapidemo.api
+
+import com.squareup.moshi.Json
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface HeartThisAtApi {
+
+    // https://api-v2.hearthis.at/feed/?type=popular&page=1&count=5
+
+    @GET("/feed")
+    suspend fun feed(
+        @Query("type") type: TypeJson?,
+        @Query("page") page: Int,
+        @Query("count") count: Int
+    ): List<FeedItemJson>
+
+    @GET("/{permalink}")
+    suspend fun artist(@Path("permalink") permalink: String): ArtistJson
+
+}
+
+enum class TypeJson {
+    @Json(name = "popular")
+    POPULAR,
+    @Json(name = "new")
+    NEW
+}
+
+data class FeedItemJson(
+    val id: String,
+    @Json(name = "user_id") val userId: String,
+    val user: UserJson
+)
+
+data class UserJson(
+    val id: String,
+    val permalink: String
+)
+
+data class ArtistJson(
+    val id: String,
+    val permalink: String,
+    val username: String,
+    @Json(name = "avatar_url") val avatar_url: String,
+    @Json(name = "background_url") val background_url: String,
+    val description: String,
+    @Json(name = "track_count") val track_count: Int,
+    @Json(name = "followers_count") val followers_count: Long
+)
